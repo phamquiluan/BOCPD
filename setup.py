969ac836f3dd7350732988bedf6ef3d2bcb8b71f
Luan Pham <phamquiluan@gmail.com>
2023-08-28 04:27:09 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-
 from setuptools import setup
 
 import bayesian_changepoint_detection
@@ -12,10 +10,5 @@ setup(
     author_email='mail@johanneskulick.net',
     url='http://github.com/hildensia/bayesian_changepoint_detection',
     packages=['bayesian_changepoint_detection'],
-    install_requires=['scipy', 'numpy', 'decorator'],
-    extras_require={
-        'dev': ['pytest'],
-        'multivariate': ['scipy>=1.6.0'],
-        'plot': ['matplotlib'],
-    }
+    install_requires=['scipy>=1.6.0', 'numpy', 'decorator', 'pytest', 'matplotlib'],
 )
