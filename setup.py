e531ec5a5c088d8bf7728f251c2d05124492699a
Luan Pham <phamquiluan@gmail.com>
2023-08-28 04:31:14 +1000

    update setup

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='mail@johanneskulick.net',
     url='http://github.com/hildensia/bayesian_changepoint_detection',
     packages=['bayesian_changepoint_detection'],
-    install_requires=['scipy>=1.6.0', 'numpy', 'decorator', 'pytest', 'matplotlib'],
+    install_requires=['scipy>=1.6.0', 'numpy', 'decorator', 'pytest', 'matplotlib', 'seaborn', 'tqdm'],
 )
